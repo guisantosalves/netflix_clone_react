496cfa5bd8447bf53e095b705e586ec1783801ce
guisantosalves <workguisantos@gmail.com>
2022-05-31 12:35:44 -0400

    setting request

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from '../axios'
 
-function Row({title}){
+function Row({title, fetchUrl}){
 
     const [movies, setMovies] = useState([]);
+
+    useEffect(()=>{
+        //make a request only once
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            console.log(request);
+            return request;
+        }
+        fetchData();
+    }, []);
 
     return(
         <div>
             {/* title */}
             <h2>{title}</h2>
+
             {/* container -> posters */}
 
             {/*  */}
