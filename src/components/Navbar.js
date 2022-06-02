105dffacfd5cd3d4307cfc9b7b9f7f9640d5b126
guisantosalves <workguisantos@gmail.com>
2022-06-02 10:26:47 -0400

    making navbar animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./css/Navbar.css";
 
 function Navbar(props) {
-  return (
-    <div className="nav">
+  const [show, handleShow] = useState(false);
+    
+  //verifying y-axis of scroll
+  useEffect(() => {
 
+    window.addEventListener("scroll", () => {
+        
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+
+    });
+
+    return () => {
+      window.removeEventListener("scroll", null);
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
       <img
         className="nav__logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/NETFLIX_logo.svg/640px-NETFLIX_logo.svg.png"
         alt="netflix logo"
       />
 
-      <img 
+      <img
         className="nav__avatar"
         src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
         alt="netflix logo"
